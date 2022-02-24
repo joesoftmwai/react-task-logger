f00e79d3e1672e138ced5058be1f59fd04691e76
Mwai Joseph <joesoftmwai@gmail.com>
2022-02-24 11:31:08 +0300

    initialized components structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className="container">
+      <Header />
       <h1>You will never walk alone</h1>
       <h2>{name}</h2>
     </div>
