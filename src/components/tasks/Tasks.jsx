7a46d53073f6d79544329eae0632b8d14946d790
Mwai Joseph <joesoftmwai@gmail.com>
2022-02-24 15:53:30 +0300

    utilized useState _hook

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-let _tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        date: 'Feb 5th @ 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Arsenal vs wolves matdh day',
-        date: 'Feb 24th @ 10:45pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'New gig from kelvin',
-        date: 'Feb 26th @ 10:30pm',
-        reminder: true
-    }
-]
+import { useState } from 'react'
+
 const Tasks = () => {
+
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: 'Doctors Appointment',
+                date: 'Feb 5th @ 2:30pm',
+                reminder: true
+            },
+            {
+                id: 2,
+                text: 'Arsenal vs wolves matdh day',
+                date: 'Feb 24th @ 10:45pm',
+                reminder: true
+            },
+            {
+                id: 3,
+                text: 'New gig from kelvin',
+                date: 'Feb 26th @ 10:30pm',
+                reminder: true
+            }
+        ]
+    )
+
   return (
     <div>
         {
-         _tasks.map(_task => (
+         tasks.map(_task => (
             <h5 key={task.id}> {_task.text} </h5> 
             )) 
         }
